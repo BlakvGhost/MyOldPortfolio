25cf17c2eb9c58afae2ea2413a02dfe5b5f8c90a
Elfried <86885681+BlakvGhost@users.noreply.github.com>
2022-11-27 19:10:10 +0100

    first commit

---
+++ assets/js/app.vue.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            data: {},
+            lang: "fr",
+        };
+    },
+
+    methods: {
+        getInfo() {
+            axios
+                .get(`admin/const.data.json`)
+                .then((res) => this.data = { ...this.data, ...res.data });
+            axios
+                .get(`admin/lang/${this.lang}.json`)
+                .then((res) => this.data = { ...this.data, ...res.data })
+        },
+        select() {
+            location.assign(`?${this.lang}`);
+        },
+    },
+
+    mounted() {
+        this.lang =
+            location.search.length <= 1 ? "fr" : location.search.split("?")[1];
+        this.getInfo();
+    },
+});
+app.mount("#app");
