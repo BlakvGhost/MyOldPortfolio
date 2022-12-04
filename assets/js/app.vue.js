27f7c36dd2581eb1f6ef4c1111fe8b7344344138
Kabirou ALASSANE <86885681+BlakvGhost@users.noreply.github.com>
2022-12-04 23:47:47 +0000

    updated method add

--- assets/js/app.vue.js
+++ assets/js/app.vue.js
@@ -24,6 +24,28 @@ const app = Vue.createApp({
         this.lang =
             location.search.length <= 1 ? "fr" : location.search.split("?")[1];
         this.getInfo();
-    },    
+    },
+    updated() {
+        $(".isotope-wrapper").each(function() {
+            var $isotope = $(".isotope-box", this);
+            var $filterCheckboxes = $('input[type="radio"]', this);
+        
+            var filter = function() {
+              var type = $filterCheckboxes.filter(":checked").data("type") || "*";
+              if (type !== "*") {
+                type = '[data-type="' + type + '"]';
+              }
+              $isotope.isotope({ filter: type });
+            };
+        
+            $isotope.isotope({
+              itemSelector: ".isotope-item",
+              layoutMode: "masonry"
+            });
+        
+            $(this).on("change", filter);
+            filter();
+          });
+    }
 });
 app.mount("#app");
