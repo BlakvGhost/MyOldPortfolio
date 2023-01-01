78a85dcaaed331bc5f15c893c4444f0e99a003c6
BlakvGhost <kabirou2001@gmail.com>
2023-01-01 23:48:35 +0000

    second commit

--- assets/js/app.vue.js
+++ assets/js/app.vue.js
@@ -25,5 +25,15 @@ const app = Vue.createApp({
             location.search.length <= 1 ? "fr" : location.search.split("?")[1];
         this.getInfo();
     },
+    updated() {
+        new fJs.Intersection({
+            elt: '.revealElement',
+            class: "animate__animated animate__pulse opacity-100",
+            root: null,
+            ratio: 0.2,
+            rootMargin: '0px',
+            threshold: 0.7,
+        });
+    }
 });
 app.mount("#app");
